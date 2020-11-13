2622007644c1de9a4ff6befb044168fc23905e34
红薯 <liudong@oschina.cn>
2020-11-13 11:37:36 +0800

    完成关键字高亮、关键字提取并测试完美

--- core/src/main/java/com/gitee/search/core/SearchHelper.java
+++ core/src/main/java/com/gitee/search/core/SearchHelper.java
@@ -1,10 +1,24 @@
 package com.gitee.search.core;
 
 import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.document.*;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.highlight.*;
+import org.lionsoul.jcseg.IWord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.lionsoul.jcseg.segmenter.*;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -17,6 +31,109 @@ import java.util.stream.Collectors;
 public class SearchHelper {
 
     private final static Logger log = LoggerFactory.getLogger(SearchHelper.class);
+
+    public static void main(String[] args) {
+        String text = "Gitee Search是Gitee的搜索引擎服务模块，为Gitee提供仓库、Issue、代码等搜索服务。";
+        System.out.println(highlight(text, "gitee search 仓库"));
+        //splitKeywords(text).forEach(e -> System.out.println(e));
+        //System.out.println(cleanupKey(text));
+    }
+
+    /**
+     * 生成查询条件
+     *
+     * @param field
+     * @param q
+     * @param boost
+     * @return
+     */
+    public static Query makeQuery(String field, String q, float boost) {
+        QueryParser parser = new QueryParser(field, JcsegAnalyzer.INSTANCE);
+        parser.setDefaultOperator(QueryParser.AND_OPERATOR);
+        try {
+            Query querySinger = parser.parse(q);
+            //System.out.println(querySinger.toString());
+            return querySinger;
+        } catch (Exception e) {
+            return new TermQuery(new Term(field, q));
+        }
+    }
+
+    /**
+     * 关键字切分
+     *
+     * @param sentence 要分词的句子
+     * @return 返回分词结果
+     */
+    public static List<String> splitKeywords(String sentence) {
+        List<String> keys = new ArrayList<String>();
+        if (StringUtils.isNotBlank(sentence)) {
+            StringReader reader = new StringReader(sentence);
+            ComplexSeg ikseg = new ComplexSeg(JcsegAnalyzer.INSTANCE.getSegmenterConfigForSearch(), JcsegAnalyzer.INSTANCE.getDic());
+            //NLPSeg ikseg = new NLPSeg(JcsegAnalyzer.INSTANCE.getSegmenterConfigForSearch(), JcsegAnalyzer.INSTANCE.getDic());
+            //DelimiterSeg ikseg = new DelimiterSeg(JcsegAnalyzer.INSTANCE.getSegmenterConfigForSearch(), JcsegAnalyzer.INSTANCE.getDic());
+            //MostSeg ikseg = new MostSeg(JcsegAnalyzer.INSTANCE.getSegmenterConfigForSearch(), JcsegAnalyzer.INSTANCE.getDic());
+            //SimpleSeg ikseg = new SimpleSeg(JcsegAnalyzer.INSTANCE.getSegmenterConfigForSearch(), JcsegAnalyzer.INSTANCE.getDic());
+            //NGramSeg ikseg = new NGramSeg(JcsegAnalyzer.INSTANCE.getSegmenterConfigForSearch(), JcsegAnalyzer.INSTANCE.getDic());
+            //DetectSeg ikseg = new DetectSeg(JcsegAnalyzer.INSTANCE.getSegmenterConfigForSearch(), JcsegAnalyzer.INSTANCE.getDic());
+            try {
+                ikseg.reset(reader);
+                do {
+                    IWord word = ikseg.next();
+                    if (word == null)
+                        break;
+                    String term = word.getValue();
+                    //if(term.length() == 1)
+                    //    continue;
+                    //if(StringUtils.isNumeric(StringUtils.remove(term,'.')))
+                    //    continue;
+                    keys.add(term);
+                } while (true);
+            } catch (IOException e) {
+                log.error("Unable to split keywords", e);
+            }
+        }
+
+        return keys.stream().distinct().collect(Collectors.toList());
+    }
+
+    /**
+     * 重整搜索关键短语
+     *
+     * @param key
+     * @return
+     */
+    public static String cleanupKey(String key) {
+        return String.join(" ", splitKeywords(key));
+    }
+
+    /**
+     * 对一段文本执行语法高亮处理
+     *
+     * @param text 要处理高亮的文本
+     * @param key  高亮的关键字
+     * @return 返回格式化后的HTML文本
+     */
+    public static String highlight(String text, String key) {
+        if (StringUtils.isBlank(key) || StringUtils.isBlank(text))
+            return text;
+
+        String result = null;
+
+        try {
+            QueryParser parser = new QueryParser(null, JcsegAnalyzer.INSTANCE);
+            Query query = parser.parse(key);
+            QueryScorer scorer = new QueryScorer(query);
+            Formatter fmt = new SimpleHTMLFormatter("<em class='highlight'>", "</em>");
+            Highlighter hig = new Highlighter(fmt, scorer);
+            TokenStream tokens = JcsegAnalyzer.INSTANCE.tokenStream(null, new StringReader(text));
+            result = hig.getBestFragment(tokens, text);
+        } catch (Exception e) {
+            log.error("Unabled to hightlight text", e);
+        }
+
+        return (result != null) ? result : text;
+    }
 
     /**
      * get document id
