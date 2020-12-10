9ee04ea4b4ea726af904e70dca31a7d225d3c9ce
红薯 <liudong@oschina.cn>
2020-12-10 16:32:04 +0800

    no copy gateway & indexer jar to lib

--- bin/indexer.bat
+++ bin/indexer.bat
@@ -1,2 +1,2 @@
 @echo off
-java -cp lib\* com.gitee.search.indexer.ServerDaemon %1 %2 %3 %4 %5 %6
+java -cp lib\*;indexer/target/classes com.gitee.search.indexer.ServerDaemon %1 %2 %3 %4 %5 %6
--- bin/indexer.sh
+++ bin/indexer.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ava -cp lib/* com.gitee.search.indexer.ServerDaemon %1 %2 %3 %4 %5 %6
+ava -cp lib/*:indexer/target/classes com.gitee.search.indexer.ServerDaemon %1 %2 %3 %4 %5 %6
