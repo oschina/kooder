5640e24d9263b460d3b7d15fe1bce90c97fe1f1d
红薯 <liudong@oschina.cn>
2020-12-16 13:48:50 +0800

    fix linux param pass

--- bin/gsimport.sh
+++ bin/gsimport.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp lib/*:indexer/target/classes com.gitee.search.indexer.PathImporter %1 %2 %3 %4 %5 %6
+java -cp lib/*:indexer/target/classes com.gitee.search.indexer.PathImporter $*
