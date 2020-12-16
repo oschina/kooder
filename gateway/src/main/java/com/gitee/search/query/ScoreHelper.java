2e83782694bba7f5e3b242971882f00238929395
红薯 <liudong@oschina.cn>
2020-12-16 10:56:43 +0800

    adjust scorer

--- gateway/src/main/java/com/gitee/search/query/ScoreHelper.java
+++ gateway/src/main/java/com/gitee/search/query/ScoreHelper.java
@@ -25,14 +25,18 @@ public class ScoreHelper {
                 score -= 10;
             //Star 数加权
             if(stars >= 100)
-                score += (stars/((recomm>0)?100:1000));
-            else if(stars <= 20)
+                score += (stars/((recomm>0)?10:100));
+            else if(stars < 10)
                 score -= 10;
+
             //Gitee Index 加权
             if(gindex > 80)
                 score += 15;
             else if(gindex > 50)
                 score += 10;
+
+            if(score < 0)
+                score = 1;
         }
         //System.out.println(" new score: " + score);
         return score;
