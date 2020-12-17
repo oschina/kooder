efcf3961365910321f6732608ac4c6bec08e1167
红薯 <liudong@oschina.cn>
2020-12-17 20:52:55 +0800

    example

---
+++ gateway/src/main/java/com/gitee/search/examples/CollectorExample.java
@@ -0,0 +1,39 @@
+package com.gitee.search.examples;
+
+import com.gitee.search.storage.StorageFactory;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.search.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * Collector example
+ * @author Winter Lau<javayou@gmail.com>
+ */
+public class CollectorExample {
+
+    private final static Logger log = LoggerFactory.getLogger(CollectorExample.class);
+
+    public static void main(String[] args) throws IOException {
+        try (IndexReader reader = StorageFactory.getIndexReader("repo")) {
+            IndexSearcher searcher = new IndexSearcher(reader);
+            long ct = System.currentTimeMillis();
+            searcher.search(new MatchAllDocsQuery(), new SimpleCollector() {
+                @Override
+                public ScoreMode scoreMode() {
+                    return ScoreMode.COMPLETE;
+                }
+
+                @Override
+                public void collect(int doc) throws IOException {
+                    log.info("{}", doc);
+                }
+            });
+        }
+        SimpleCollector d;
+    }
+
+}
