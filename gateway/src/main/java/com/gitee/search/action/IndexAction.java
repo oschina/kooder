0f0977c5841315bb0099c4a8aa16203782e729ae
红薯 <liudong@oschina.cn>
2021-01-17 14:54:43 +0800

    action refine

--- gateway/src/main/java/com/gitee/search/action/IndexAction.java
+++ gateway/src/main/java/com/gitee/search/action/IndexAction.java
@@ -1,15 +1,21 @@
 package com.gitee.search.action;
 
-import com.gitee.search.server.Action;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.vertx.core.http.HttpServerRequest;
 import io.vertx.ext.web.RoutingContext;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Default action for web
  * @author Winter Lau<javayou@gmail.com>
  */
-public class IndexAction implements Action {
+public class IndexAction implements SearchActionBase {
 
     /**
      * web searcher
@@ -18,6 +24,35 @@ public class IndexAction implements Action {
      */
     public void index(RoutingContext context) throws IOException {
         this.vm(context, "index.vm");
+    }
+
+    /**
+     * controller for search.vm
+     * @param context
+     * @throws IOException
+     */
+    public void search(RoutingContext context) throws IOException {
+
+        HttpServerRequest request = context.request();
+        String q = param(request, "q");
+        if(StringUtils.isBlank(q)) {
+            this.redirect(context, "/");
+            return ;
+        }
+
+        String type = param(request,"type", "repo");
+
+        String json = _search(request, type);
+        if(json == null) {
+            error(context.response(), HttpResponseStatus.BAD_REQUEST.code(), "Illegal parameter 'type' value.");
+            return;
+        }
+
+        Map<String, Object> params = new HashMap();
+        JsonNode node = new ObjectMapper().readTree(json);
+        params.put("result", node);
+
+        this.vm(context, "search.vm", params);
     }
 
 }
