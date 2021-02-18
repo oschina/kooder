2f4a0ab18e54e5dc5247427a07f1b203c74fcfd1
红薯 <liudong@oschina.cn>
2021-02-18 13:46:46 +0800

    add license for each source file

--- core/src/main/java/com/gitee/kooder/index/IndexManager.java
+++ core/src/main/java/com/gitee/kooder/index/IndexManager.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2021, OSChina (oschina.net@gmail.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.gitee.kooder.index;
 
 import com.gitee.kooder.core.Constants;
@@ -23,7 +38,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * 索引管理器
+ * kooder index mananger
  * @author Winter Lau<javayou@gmail.com>
  */
 public class IndexManager {
--- core/src/main/java/com/gitee/kooder/query/QueryBase.java
+++ core/src/main/java/com/gitee/kooder/query/QueryBase.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2021, OSChina (oschina.net@gmail.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.gitee.kooder.query;
 
 import com.gitee.kooder.core.AnalyzerFactory;
@@ -9,7 +24,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.IntPoint;
-import org.apache.lucene.document.NumericDocValuesField;
 import org.apache.lucene.facet.*;
 import org.apache.lucene.facet.taxonomy.FastTaxonomyFacetCounts;
 import org.apache.lucene.facet.taxonomy.TaxonomyReader;
@@ -28,7 +42,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- * 搜索基类
+ * base class of query
  * @author Winter Lau<javayou@gmail.com>
  */
 public abstract class QueryBase implements IQuery {
