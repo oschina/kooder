8d69f85f03fc7d65cd800a30266286dde90c8e77
红薯 <liudong@oschina.cn>
2021-03-12 16:07:23 +0800

    don't install hook while http.url is empty

--- indexer/src/main/java/com/gitee/kooder/indexer/GitlabIndexThread.java
+++ indexer/src/main/java/com/gitee/kooder/indexer/GitlabIndexThread.java
@@ -22,6 +22,7 @@ import com.gitee.kooder.models.Issue;
 import com.gitee.kooder.models.Repository;
 import com.gitee.kooder.query.QueryFactory;
 import com.gitee.kooder.queue.QueueTask;
+import org.apache.commons.lang3.StringUtils;
 import org.gitlab4j.api.*;
 import org.gitlab4j.api.models.*;
 import org.slf4j.Logger;
@@ -92,16 +93,18 @@ public class GitlabIndexThread extends Thread {
      * @throws GitLabApiException
      */
     private void checkAndInstallSystemHook(GitLabApi gitlab) throws GitLabApiException {
-        try {
-            SystemHooksApi api = gitlab.getSystemHooksApi();
-            for (SystemHook hook : api.getSystemHooks()) {
-                if (hook.getUrl().equals(system_hook_url))
-                    return;
+        if(StringUtils.isNotBlank(gsearch_url)) {
+            try {
+                SystemHooksApi api = gitlab.getSystemHooksApi();
+                for (SystemHook hook : api.getSystemHooks()) {
+                    if (hook.getUrl().equals(system_hook_url))
+                        return;
+                }
+                gitlab.getSystemHooksApi().addSystemHook(system_hook_url, secret_token, true, true, false);
+                log.info("Gitlab system hook : {} installed.", system_hook_url);
+            } catch (Exception e) {
+                log.error("Failed to install gitlab system hook: {}", system_hook_url);
             }
-            gitlab.getSystemHooksApi().addSystemHook(system_hook_url, secret_token, true, true, false);
-            log.info("Gitlab system hook : {} installed.", system_hook_url);
-        } catch (Exception e) {
-            log.error("Failed to install gitlab system hook: {}", system_hook_url );
         }
     }
 
@@ -112,21 +115,23 @@ public class GitlabIndexThread extends Thread {
      * @throws GitLabApiException
      */
     private void checkAndInstallProjectHook(GitLabApi gitlab, Project p) throws GitLabApiException {
-        try {
-            for (ProjectHook hook : gitlab.getProjectApi().getHooks(p.getId())) {
-                if (hook.getUrl().equals(this.project_hook_url))
-                    return;
+        if(StringUtils.isNotBlank(gsearch_url)) {
+            try {
+                for (ProjectHook hook : gitlab.getProjectApi().getHooks(p.getId())) {
+                    if (hook.getUrl().equals(this.project_hook_url))
+                        return;
+                }
+                ProjectHook hook = new ProjectHook(); //just accept issue event, other event via system hook trigger
+                hook.setIssuesEvents(true);
+                hook.setPushEvents(false);
+                hook.setMergeRequestsEvents(false);
+                hook.setRepositoryUpdateEvents(false);
+                hook.setTagPushEvents(false);
+                hook.setWikiPageEvents(false);
+                gitlab.getProjectApi().addHook(p.getId(), this.project_hook_url, hook, true, secret_token);
+            } catch (Exception e) {
+                log.error("Failed to install gitlab project hook: {}", this.project_hook_url);
             }
-            ProjectHook hook = new ProjectHook(); //just accept issue event, other event via system hook trigger
-            hook.setIssuesEvents(true);
-            hook.setPushEvents(false);
-            hook.setMergeRequestsEvents(false);
-            hook.setRepositoryUpdateEvents(false);
-            hook.setTagPushEvents(false);
-            hook.setWikiPageEvents(false);
-            gitlab.getProjectApi().addHook(p.getId(), this.project_hook_url, hook, true, secret_token);
-        } catch (Exception e) {
-            log.error("Failed to install gitlab project hook: {}", this.project_hook_url);
         }
     }
 
