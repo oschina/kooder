2b78510e8a79e275f9b75587036b6dba5cfac55d
silverballer <aa2323326>
2021-04-01 15:21:17 +0800

    fix: default ID of the repo imported from the file is the same

--- indexer/src/main/java/com/gitee/kooder/file/FileIndexThread.java
+++ indexer/src/main/java/com/gitee/kooder/file/FileIndexThread.java
@@ -5,6 +5,7 @@ import com.gitee.kooder.core.KooderConfig;
 import com.gitee.kooder.models.CodeRepository;
 import com.gitee.kooder.models.Repository;
 import com.gitee.kooder.queue.QueueTask;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +13,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * index from file
@@ -27,25 +29,30 @@ public class FileIndexThread extends Thread {
         String filePath = KooderConfig.getProperty("file.index.path");
 
         long start = System.currentTimeMillis();
+        AtomicLong id = new AtomicLong(start);
         AtomicInteger success = new AtomicInteger(), error = new AtomicInteger();
         try {
-            Files.lines(Paths.get(filePath)).forEach(repoUrl -> {
-                try {
-                    Repository repo = new Repository();
-                    repo.setName(repoUrl.substring(repoUrl.lastIndexOf("/") + 1, repoUrl.lastIndexOf(".git")));
-                    repo.setUrl(repoUrl);
-                    QueueTask.add(Constants.TYPE_REPOSITORY, repo);
-                    CodeRepository codes = new CodeRepository();
-                    codes.setScm(CodeRepository.SCM_GIT);
-                    codes.setName(repo.getName());
-                    codes.setUrl(repo.getUrl());
-                    QueueTask.add(Constants.TYPE_CODE, codes);
-                    success.incrementAndGet();
-                } catch (Exception e) {
-                    error.incrementAndGet();
-                    log.warn("{} index error:", repoUrl, e.getMessage());
-                }
-            });
+            Files.lines(Paths.get(filePath))
+                    .filter(StringUtils::isNotBlank)
+                    .forEach(repoUrl -> {
+                        try {
+                            Repository repo = new Repository();
+                            repo.setId(id.getAndIncrement());
+                            repo.setName(repoUrl.substring(repoUrl.lastIndexOf("/") + 1, repoUrl.lastIndexOf(".git")));
+                            repo.setUrl(repoUrl);
+                            QueueTask.add(Constants.TYPE_REPOSITORY, repo);
+                            CodeRepository codes = new CodeRepository();
+                            codes.setId(repo.getId());
+                            codes.setScm(CodeRepository.SCM_GIT);
+                            codes.setName(repo.getName());
+                            codes.setUrl(repo.getUrl());
+                            QueueTask.add(Constants.TYPE_CODE, codes);
+                            success.incrementAndGet();
+                        } catch (Exception e) {
+                            error.incrementAndGet();
+                            log.warn("{} index error:", repoUrl, e.getMessage());
+                        }
+                    });
             log.info(
                     "index from file: {} repositories indexed, success: {}, error: {}, using {} ms",
                     success.get() + error.get(),
