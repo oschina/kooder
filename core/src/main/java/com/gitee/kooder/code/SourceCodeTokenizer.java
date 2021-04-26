4df442d07762e422625a466c9c7d86c83cead51c
红薯 <liudong@oschina.cn>
2021-04-26 10:17:16 +0800

    ignore invisible token

--- core/src/main/java/com/gitee/kooder/code/SourceCodeTokenizer.java
+++ core/src/main/java/com/gitee/kooder/code/SourceCodeTokenizer.java
@@ -36,8 +36,8 @@ import java.util.List;
  */
 public class SourceCodeTokenizer extends Tokenizer {
 
-    private final static String separatorChars = " ~!@#$%^&*()-_+[]{}?/\\<>.;,'\"";
-    private final static String uselessChars = ""; //{}()[];,
+    private final static String separatorChars = " ~!@#$%^&*()-_+[]{}?/\\<>.;,'\"\r\n\t";
+    private final static String uselessChars = "\r\n\t "; //{}()[];,
 
     private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
     private final OffsetAttribute offsetAtt = addAttribute(OffsetAttribute.class);
